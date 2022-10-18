7cc1a89bf014df9a17ac8d2a64e49246015a1407
Adam Dickinson <adamgamesyt@gmail.com>
2022-10-18 13:47:41 +0200

    Created Provider Class

---
+++ SEN_Final_Project_Last_Time_Doing_This/SEN_381_Final_Project/BLL/Provider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEN_381_Final_Project.BLL
+{
+    class Provider
+    {
+        private string company_ID;
+        private string company_Name;
+        private string location;
+        private string address;
+        private string status;
+
+        public string Company_ID { get => company_ID; set => company_ID = value; }
+        public string Company_Name { get => company_Name; set => company_Name = value; }
+        public string Location { get => location; set => location = value; }
+        public string Address { get => address; set => address = value; }
+        public string Status { get => status; set => status = value; }
+    }
+}
