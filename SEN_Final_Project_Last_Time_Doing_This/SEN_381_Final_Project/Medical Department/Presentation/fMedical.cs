905959781ea2e14c1786059ca34592bb1ceeaeed
Adam Dickinson <adamgamesyt@gmail.com>
2022-11-02 14:10:45 +0200

    Created medical form

---
+++ SEN_Final_Project_Last_Time_Doing_This/SEN_381_Final_Project/Medical Department/Presentation/fMedical.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SEN_381_Final_Project.Medical_Department.Presentation
+{
+    public partial class fMedical : Form
+    {
+        public fMedical()
+        {
+            InitializeComponent();
+        }
+    }
+}
